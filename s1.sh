da2fb3d79b1c0e00e9cdf48f8e030e1a0edf4862
Emmanuel <emmanuel_delgado@ciencias.unam.mx>
2018-07-24 13:02:54 -0500

    Salvando los scripts

---
+++ s1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Inicia el script1"
+ls
+mkdir magia
+touch ./magia/alibaba
+echo "hocus pocus" > ./magia/alibaba
+cat alibaba
+echo "Fin del script 1"
---
+++ s2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Inicia el Script 2"
+./s1.sh
+touch $1
+ls
+mv $1 
